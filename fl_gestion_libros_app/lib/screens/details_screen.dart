23c13f22c18d1208afed4e655770f11cb2ce8b51
MartaRondan96 <martarondan96@gmail.com>
2023-05-17 22:09:15 +0200

    17/05 v3

--- fl_gestion_libros_app/lib/screens/details_screen.dart
+++ fl_gestion_libros_app/lib/screens/details_screen.dart
@@ -25,6 +25,8 @@ class _DetailsScreen_state extends State<DetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
+        final TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       body: FutureBuilder<Libro>(
         future: getLibro(),
@@ -67,9 +69,57 @@ class _DetailsScreen_state extends State<DetailsScreen> {
     ),
                 SliverList(
                   delegate: SliverChildListDelegate([
-                    _PosterAndTitle(),
-                    _OverView(),
-                    CastingCards(),
+                    //Poster and title
+                    Container(
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: FadeInImage(
+                            placeholder: AssetImage('assets/no-image.jpg'),
+                            image: AssetImage('assets/'+libro.imagen!),
+                            height: 200,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              libro.titulo!,
+                              style: textTheme.headline5,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                            Text(
+                              libro.autor!,
+                              style: textTheme.subtitle1,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                                SizedBox(width: 5),
+                                Text(libro.nota!.toString(), style: textTheme.caption)
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  //Resumen del libro
+                   Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    child: Text(
+                      'Esto es una prueba',
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  ),
                   ]),
                 ),
               ],
@@ -83,64 +133,64 @@ class _DetailsScreen_state extends State<DetailsScreen> {
     );
   }
 }
-class _PosterAndTitle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    return Container(
-      margin: EdgeInsets.only(top: 20),
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: FadeInImage(
-              placeholder: AssetImage('assets/no-image.jpg'),
-              image: NetworkImage('https://via.placeholder.com/200x300'),
-              height: 150,
-            ),
-          ),
-          SizedBox(width: 20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'movie.title',
-                style: textTheme.headline5,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-              Text(
-                'movie.subtitle',
-                style: textTheme.subtitle1,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-              Row(
-                children: [
-                  Icon(Icons.star_outline, size: 15, color: Colors.grey),
-                  SizedBox(width: 5),
-                  Text('movie.voteAvarage', style: textTheme.caption)
-                ],
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+// class _PosterAndTitle extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final TextTheme textTheme = Theme.of(context).textTheme;
+//     return Container(
+//       margin: EdgeInsets.only(top: 20),
+//       padding: EdgeInsets.symmetric(horizontal: 20),
+//       child: Row(
+//         children: [
+//           ClipRRect(
+//             borderRadius: BorderRadius.circular(20),
+//             child: FadeInImage(
+//               placeholder: AssetImage('assets/no-image.jpg'),
+//               image: NetworkImage('https://via.placeholder.com/200x300'),
+//               height: 150,
+//             ),
+//           ),
+//           SizedBox(width: 20),
+//           Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text(
+//                 'movie.title',
+//                 style: textTheme.headline5,
+//                 overflow: TextOverflow.ellipsis,
+//                 maxLines: 2,
+//               ),
+//               Text(
+//                 'movie.subtitle',
+//                 style: textTheme.subtitle1,
+//                 overflow: TextOverflow.ellipsis,
+//                 maxLines: 2,
+//               ),
+//               Row(
+//                 children: [
+//                   Icon(Icons.star_outline, size: 15, color: Colors.grey),
+//                   SizedBox(width: 5),
+//                   Text('movie.voteAvarage', style: textTheme.caption)
+//                 ],
+//               ),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
 
-class _OverView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-      child: Text(
-        'Esto es una prueba',
-        textAlign: TextAlign.justify,
-        style: Theme.of(context).textTheme.subtitle1,
-      ),
-    );
-  }
-}
+// class _OverView extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+//       child: Text(
+//         'Esto es una prueba',
+//         textAlign: TextAlign.justify,
+//         style: Theme.of(context).textTheme.subtitle1,
+//       ),
+//     );
+//   }
+// }
